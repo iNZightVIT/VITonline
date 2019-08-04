a6931f6b496dab2857febd6d865942e5890461b0
ben <benhalstd@gmail.com>
2019-08-04 14:32:51 +1200

    better one num bootstrap animations

--- bootstrap/Visualisations/bootstrap_oneNum.js
+++ bootstrap/Visualisations/bootstrap_oneNum.js
@@ -121,6 +121,10 @@ class bootstrap_oneNum extends visBase {
 
 	populationDropDown(settings, currentAnimation){
 		var self = this;
+		this.showSteps = false;
+		if(settings.repititions == 1 && !settings.incDist) {
+			this.showSteps = d3.select("#trackCBox").property("checked");
+		}
         for (let s of settings.sample){
             s.upTo = settings.indexUpTo + 1;
         }
@@ -149,23 +153,39 @@ class bootstrap_oneNum extends visBase {
 
 	    this.animationController(settings, currentAnimation);
 	}
+
+
 	buildUpSlow(settings, currentAnimation, upto, popText, max, self){
 		d3.selectAll(".redHighlight").remove();
 		if(upto >= max){
+			if(self.showSteps){
+				self.trackListLines(settings, currentAnimation, 0, popText, max, self);
+				return;
+			}
 			this.animationController(settings, currentAnimation);
 			return;
-        }
+		}
+		let base_speed= (upto < 10 ? 500 : 250);
         let selected_sample_item = settings.sample[upto];
         d3.selectAll("#sampleReRandomised text").style("fill", "black")
             .attr("x", function(){return d3.select(this).attr("data-x") ? d3.select(this).attr("data-x") : d3.select(this).attr("x")})
             .attr("y", function(){return d3.select(this).attr("data-y") ? d3.select(this).attr("data-y") : d3.select(this).attr("y")});
 		popText.append("text").attr("class", "redHighlight").text(self.allPop[selected_sample_item.id].value).attr("x",self.windowHelper.sampleSection.S1.x + self.windowHelper.sampleSection.S1.width * (2/4)).attr("y", selected_sample_item.id < 58 ? (self.windowHelper.sampleSection.S1.y + self.windowHelper.fontSize + (self.windowHelper.fontSize+2)*(selected_sample_item.id+1)) : -10).style("font-size",self.windowHelper.fontSize).style("display","inline-block").attr("text-anchor","middle").style("fill", "red");
-		d3.selectAll(".pop .c"+selected_sample_item.id).attr("stroke-opacity", 1).attr("fill-opacity", 1).transition().duration(500/settings.repititions).each('end', function(d, i){
+		d3.selectAll(".pop .c"+selected_sample_item.id).attr("stroke-opacity", 1).attr("fill-opacity", 1).transition().duration(base_speed/settings.repititions).each('end', function(d, i){
             if(i == 0){
                 self.buildUpSlow(settings, currentAnimation, upto+1, popText, max, self);
             }
-        });
-        d3.select("#circleOverlay .c"+selected_sample_item.order).attr("stroke-opacity", 1).attr("fill-opacity", 1);
+		});
+		
+		if(this.showSteps){
+			d3.select("#circleOverlay .c"+selected_sample_item.order).attr("stroke-opacity", 1).attr("fill-opacity", 1)
+			.attr("data-cy", function(){return d3.select(this).attr("cy")})
+			.attr("cy", function(){return d3.select(".pop .c"+selected_sample_item.id).attr("cy")})
+			.transition().duration(base_speed/settings.repititions)
+			.attr("cy", function(){return d3.select(this).attr("data-cy")});
+		}else{
+			d3.select("#circleOverlay .c"+selected_sample_item.order).attr("stroke-opacity", 1).attr("fill-opacity", 1);
+		}
         
         
         if(upto < 10){
@@ -175,7 +195,7 @@ class bootstrap_oneNum extends visBase {
             .attr("data-y", function(){return d3.select(this).attr("y")})
             .attr("x",self.windowHelper.sampleSection.S1.x + self.windowHelper.sampleSection.S1.width * (2/4)).attr("y", (self.windowHelper.sampleSection.S1.y + self.windowHelper.fontSize + (self.windowHelper.fontSize+2)*(selected_sample_item.id+1)))
             .style("opacity", 1).style("fill", "red")
-            .transition().duration(500/settings.repititions)
+            .transition().duration(base_speed/settings.repititions)
             .attr("x", function(){return d3.select(this).attr("data-x") ? d3.select(this).attr("data-x") : d3.select(this).attr("x")})
             .attr("y", function(){return d3.select(this).attr("data-y") ? d3.select(this).attr("data-y") : d3.select(this).attr("y")});
         }else{
@@ -213,6 +233,69 @@ class bootstrap_oneNum extends visBase {
 		}
 
 
+	}
+
+	trackListLines(settings, currentAnimation, upto, popText, max, self){
+		d3.selectAll(".redHighlight").remove();
+		if(upto >= max){
+			this.animationController(settings, currentAnimation);
+			return;
+		}
+		let pop_item = self.allPop[upto];
+        let selected_sample_items = settings.sample.filter((x) => (x.id == pop_item.id));
+        d3.selectAll("#sampleReRandomised text").style("fill", "black")
+            .attr("x", function(){return d3.select(this).attr("data-x") ? d3.select(this).attr("data-x") : d3.select(this).attr("x")})
+			.attr("y", function(){return d3.select(this).attr("data-y") ? d3.select(this).attr("data-y") : d3.select(this).attr("y")});
+		
+		
+		let pop_highlight = popText.append("text").attr("class", "redHighlight").text(self.allPop[pop_item.id].value)
+			.attr("x",self.windowHelper.sampleSection.S1.x + self.windowHelper.sampleSection.S1.width * (2/4))
+			.attr("y", pop_item.id < 58 ? (self.windowHelper.sampleSection.S1.y + self.windowHelper.fontSize + (self.windowHelper.fontSize+2)*(pop_item.id+1)) : -10)
+			.style("font-size",self.windowHelper.fontSize).style("display","inline-block")
+			.attr("text-anchor","middle")
+			.style("fill", "red");
+		
+		for (let ssi of selected_sample_items){
+			let sample_element = d3.select(".t"+ssi.order);
+			sample_element.style("opacity", 1).style("fill", "red");
+			
+			let sample_e_bbox = sample_element.node().getBBox();
+			let pop_e_bbox = pop_highlight.node().getBBox();
+			popText.append("line").attr("class", "redHighlight")
+			.attr("y1", pop_e_bbox.y + (pop_e_bbox.height / 2)).attr("x1", pop_e_bbox.x + (pop_e_bbox.width))
+			.attr("y2", sample_e_bbox.y + (sample_e_bbox.height / 2)).attr("x2", sample_e_bbox.x + (sample_e_bbox.width * 0))
+			.style("stroke", "red");
+		}
+
+		d3.selectAll(".pop .c"+pop_item.id).attr("stroke-opacity", 1).attr("fill-opacity", 1).transition().duration(500/settings.repititions).each('end', function(d, i){
+            if(i == 0){
+                self.trackListLines(settings, currentAnimation, upto+1, popText, max, self);
+            }
+		});
+		
+		// if(this.showSteps){
+		// 	d3.select("#circleOverlay .c"+selected_sample_item.order).attr("stroke-opacity", 1).attr("fill-opacity", 1)
+		// 	.attr("data-cy", function(){return d3.select(this).attr("cy")})
+		// 	.attr("cy", function(){return d3.select(".pop .c"+selected_sample_item.id).attr("cy")})
+		// 	.transition().duration(500/settings.repititions)
+		// 	.attr("cy", function(){return d3.select(this).attr("data-cy")});
+		// }else{
+		// 	d3.select("#circleOverlay .c"+selected_sample_item.order).attr("stroke-opacity", 1).attr("fill-opacity", 1);
+		// }
+        
+        
+        // if(upto < 10){
+        //     d3.selectAll(".t"+selected_sample_item.order)
+        //     .attr("data-x", function(){return d3.select(this).attr("x")})
+        //     .attr("data-y", function(){return d3.select(this).attr("y")})
+        //     .attr("x",self.windowHelper.sampleSection.S1.x + self.windowHelper.sampleSection.S1.width * (2/4)).attr("y", (self.windowHelper.sampleSection.S1.y + self.windowHelper.fontSize + (self.windowHelper.fontSize+2)*(selected_sample_item.id+1)))
+        //     .style("opacity", 1).style("fill", "red")
+        //     .transition().duration(500/settings.repititions)
+        //     .attr("x", function(){return d3.select(this).attr("data-x") ? d3.select(this).attr("data-x") : d3.select(this).attr("x")})
+        //     .attr("y", function(){return d3.select(this).attr("data-y") ? d3.select(this).attr("data-y") : d3.select(this).attr("y")});
+        // }else{
+        //     d3.selectAll(".t"+selected_sample_item.order).style("opacity", 1).style("fill", "red");
+        // }	
 	}
 
 	fadeIn(settings, currentAnimation){
