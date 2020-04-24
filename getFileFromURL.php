e90652226428c8f88f69e2d480b2538d25a9927c
ben <benhalstd@gmail.com>
2020-04-24 14:12:53 +1200

    proxy script

--- getFileFromURL.php
+++ getFileFromURL.php
@@ -2,16 +2,45 @@
  	header("Access-Control-Allow-Origin: *");
  	$url = $_GET["fn"];
 
+	 
+	$aContext = array(
 
- 	// Remove all illegal characters from a url
-	$url_no_space = filter_var($url, FILTER_SANITIZE_URL);
+		'http' => array(
+
+			'proxy' => 'tcp://squid.auckland.ac.nz:3128',
+
+			'request_fulluri' => true,
+
+		),
+
+	);
+
+	$cxContext = stream_context_create($aContext);
+
+
+
 
 	// Validate url
+
 	if (!filter_var($url_no_space, FILTER_VALIDATE_URL) === false) {
-		//echo $url;
-		echo file_get_contents(preg_replace("/[^\x20-\x7E]/", "", $url));
+
+		echo file_get_contents(preg_replace("/[^\x20-\x7E]/", "", $url),False, $cxContext);
+
 		//echo "hello";
+
 	} else {
-		echo("$url is not a valid URL");
+
+		echo("$url is not a valid URL"); 
 	}
+ 	// // Remove all illegal characters from a url
+	// $url_no_space = filter_var($url, FILTER_SANITIZE_URL);
+
+	// // Validate url
+	// if (!filter_var($url_no_space, FILTER_VALIDATE_URL) === false) {
+	// 	//echo $url;
+	// 	echo file_get_contents(preg_replace("/[^\x20-\x7E]/", "", $url));
+	// 	//echo "hello";
+	// } else {
+	// 	echo("$url is not a valid URL");
+	// }
 ?>
